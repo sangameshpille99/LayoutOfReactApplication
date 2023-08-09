7ee486d1967f42fe0f571c05921f06c4b7f8c2b0
sangameshpille99 <sangameshpille99@gmail.com>
2023-08-09 16:22:48 +0530

    Attence report changed

--- src/components/pages/Attendence.jsx
+++ src/components/pages/Attendence.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Attendence = () => {
-  return (
-    <div>Attendence</div>
-  )
-}
+  return <div>Attendence Report</div>;
+};
 
-export default Attendence
+export default Attendence;
