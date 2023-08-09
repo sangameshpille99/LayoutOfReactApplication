6b9efdc95884c6ede55d27745b5c4c0273fcb8f4
sangameshpille99 <sangameshpille99@gmail.com>
2023-08-09 17:17:59 +0530

    content changed

--- src/components/pages/Attendence.jsx
+++ src/components/pages/Attendence.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Attendence = () => {
-  return <div>Attendence Report 12345</div>;
+  return <div>Attendence Report 12345684516255</div>;
 };
 
 export default Attendence;
